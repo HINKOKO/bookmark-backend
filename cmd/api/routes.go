09557a4c5840b67903e688f7f30ce8df16573620
HINKOKO <piotr62@live.fr>
2024-06-11 16:17:35 +0200

    merging contributor backend work to main

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -15,6 +15,7 @@ func (app *application) routes() http.Handler {
 	mux.Use(app.enableCORS)
 
 	mux.Get("/", app.Home)
+	// Bookmarks && resources
 	mux.Get("/bookmarks/{category}", app.GetProjectsByCategory)
 	mux.Get("/dashboard", app.Dashboard)
 	mux.Get("/auth/{provider}", app.HandleAuth)
